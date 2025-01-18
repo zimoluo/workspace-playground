3ca570b8f74923df8d12b8e7023cefc4948bc94d
zimoluo <zimo_luo@outlook.com>
2025-01-18 00:30:19 -0500

    layout should be good now

--- Views/ContentView.swift
+++ Views/ContentView.swift
@@ -3,9 +3,12 @@ import SwiftUI
 
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
-    @Query() var themes: [Theme]
+    @Query(sort: \Theme.dateCreated, order: .reverse) var themes: [Theme]
     @Environment(\.theme) private var theme
     @Environment(\.colorScheme) private var colorScheme
+
+    private let columns: [GridItem] = Array(repeating: .init(.fixed(36), spacing: 16), count: 5)
+    private let maxThemesGridHeight: CGFloat = 120
 
     var body: some View {
         NavigationSplitView {
@@ -20,35 +23,87 @@ struct ContentView: View {
                 .padding(16)
                 .background(themeColor(from: theme, for: .primary, in: colorScheme, level: 5))
 
-                VStack(spacing: 10) {
-                    let backgroundColor = themeColor(from: theme, for: .primary, in: colorScheme, level: 3)
-                    let foregroundColor = themeColor(from: theme, for: .primary, in: colorScheme, level: 0)
+                VStack(spacing: 8) {
                     SectionView(header: "Spaces") {
-                        Text("Until we meet again")
-                            .fontWeight(.semibold)
-                            .padding(16)
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                            .background(
-                                backgroundColor
-                            )
-                            .cornerRadius(16)
-                            .foregroundColor(
-                                foregroundColor
-                            )
-                            .shadow(color: theme.secondary.toShadow(opacityMultiplier: 0.8), radius: 12, y: 8)
+                        ScrollView {
+                            VStack(alignment: .leading, spacing: 10) {
+                                Text("Until we meet again")
+                                    .padding(16)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                    .background(
+                                        themeColor(from: theme, for: .primary, in: colorScheme, level: 3)
+                                    )
+                                    .cornerRadius(16)
+                                    .foregroundColor(
+                                        themeColor(from: theme, for: .primary, in: colorScheme, level: 0)
+                                    )
+                                    .shadow(color: theme.primary.toShadow(opacityMultiplier: 0.8), radius: 12, y: 8)
+                            }
+                        }
+                        .frame(maxHeight: .infinity)
                     }
 
-                    // not really a spacer down below. the "spaces" section will be having a vertical stack that takes as much space possible until it clashes with the thing below in which case it scrolls.
-                    Spacer()
+                    SectionView(header: "Themes") {
+                        ScrollView {
+                            LazyVGrid(columns: columns, spacing: 16) {
+                                ForEach(themes.filter { $0.id != theme.id }) { eachTheme in
+                                    Button(action: {
+                                        applyTheme(eachTheme.id)
+                                    }) {
+                                        Circle()
+                                            .fill(eachTheme.thumbnail)
+                                            .frame(width: 36, height: 36)
+                                            .overlay(
+                                                Circle()
+                                                    .stroke(themeColor(from: theme, for: .primary, in: .light, level: 5), lineWidth: 3)
+                                                    .frame(width: 33, height: 33))
+                                    }
+                                    .buttonStyle(PlainButtonStyle())
+                                    .scrollTransition { content, phase in
+                                        content
+                                            .opacity(phase.isIdentity ? 1 : 0)
+                                            .scaleEffect(phase.isIdentity ? 1 : 0.6)
+                                            .blur(radius: phase.isIdentity ? 0 : 10)
+                                    }
+                                }
 
-                    // make this a "Theme" section.
-                    // make a grid or a grid-like thing here with similar styling to hstack. and then place every member of themes, except the one whose id equal to theme.id, a circle for each, filled in eachTheme.thumbnail (it's a gradient). the grid will have a similar styling (bg, fg, padding, corner, shadow) to the hstack below. its max height does not exceed 240, so you need to scroll if necessary. the spacing between each grid item is 16, and each circle should have diameter 32. there will be a clicking action on the circle/button. i'll implement that part.
+                                NavigationLink(destination: ThemeMakerView()) {
+                                    Circle()
+                                        .stroke(themeColor(from: theme, for: .primary, in: colorScheme, level: 1), style: StrokeStyle(lineWidth: 4, lineCap: .round, dash: [4, 7]))
+                                        .frame(width: 32, height: 32)
+                                        .contentShape(Circle())
+                                        .overlay(Image(systemName: "plus")
+                                            .foregroundStyle(themeColor(from: theme, for: .primary, in: colorScheme, level: 1))
+                                            .fontWeight(.bold))
+                                        .scrollTransition { content, phase in
+                                            content
+                                                .opacity(phase.isIdentity ? 1 : 0)
+                                                .scaleEffect(phase.isIdentity ? 1 : 0.6)
+                                                .blur(radius: phase.isIdentity ? 0 : 10)
+                                        }
+                                }
+                            }
+                            .padding(16)
+                        }
+                        .frame(height: themesGridHeight)
+                        .background(
+                            themeColor(from: theme, for: .primary, in: colorScheme, level: 3)
+                        )
+                        .cornerRadius(16)
+                        .shadow(color: theme.primary.toShadow(opacityMultiplier: 0.8), radius: 12, y: 8)
+                    }
 
                     NavigationLink(destination: ThemeMakerView()) {
                         HStack(spacing: 12) {
                             Image(systemName: "paintpalette").font(.title2)
                                 .frame(width: 24, height: 24)
+                                .foregroundColor(
+                                    themeColor(from: theme, for: .primary, in: colorScheme, level: 1)
+                                )
                             Text("Theme Maker")
+                                .foregroundColor(
+                                    themeColor(from: theme, for: .primary, in: colorScheme, level: 0)
+                                )
                         }
                         .padding(16)
                         .frame(maxWidth: .infinity, alignment: .leading)
@@ -56,18 +111,51 @@ struct ContentView: View {
                             themeColor(from: theme, for: .primary, in: colorScheme, level: 3)
                         )
                         .cornerRadius(16)
-                        .foregroundColor(
-                            themeColor(from: theme, for: .primary, in: colorScheme, level: 0)
-                        )
-                        .shadow(color: theme.secondary.toShadow(opacityMultiplier: 0.8), radius: 12, y: 8)
+                        .shadow(color: theme.primary.toShadow(opacityMultiplier: 0.8), radius: 12, y: 8)
                     }
                 }
                 .padding(.horizontal, 16)
-                .background(LinearGradient(colors: [themeColor(from: theme, for: .primary, in: colorScheme, level: 4), themeColor(from: theme, for: .primary, in: colorScheme, level: 5)], startPoint: .bottom, endPoint: .top))
+                .background(LinearGradient(colors: [
+                    themeColor(from: theme, for: .primary, in: colorScheme, level: 4),
+                    themeColor(from: theme, for: .primary, in: colorScheme, level: 5)
+                ], startPoint: .bottom, endPoint: .top))
             }
         } detail: {
             theme.mainGradient.toGradient(in: colorScheme).ignoresSafeArea()
-        }.accentColor(themeColor(from: theme, for: .primary, in: colorScheme, level: 0))
+        }
+        .accentColor(themeColor(from: theme, for: .primary, in: colorScheme, level: 0))
+    }
+
+    private var themesGridHeight: CGFloat {
+        let filteredThemes = themes.filter { $0.id != theme.id }
+
+        let numberOfItems = filteredThemes.count + 1
+
+        let rows = ceil(Double(numberOfItems) / 5)
+
+        let totalItemHeight = CGFloat(rows) * 36
+        let totalSpacing = CGFloat(Int(rows) + 1) * 16
+
+        let requiredHeight = totalItemHeight + totalSpacing
+
+        return min(requiredHeight, maxThemesGridHeight)
+    }
+
+    private func applyTheme(_ id: UUID?) {
+        guard let id = id,
+              let matchingTheme = themes.first(where: { $0.id == id })
+        else {
+            return
+        }
+
+        guard matchingTheme.id != theme.id else { return }
+
+        let copiedTheme = matchingTheme.deepCopy()
+
+        theme.primary = copiedTheme.primary
+        theme.secondary = copiedTheme.secondary
+        theme.tertiary = copiedTheme.tertiary
+        theme.mainGradient = copiedTheme.mainGradient
     }
 }
 
