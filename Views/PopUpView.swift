b8de4fb293de09fee3219b17aaec7ebcaa0dd974
zimoluo <zimo_luo@outlook.com>
2025-01-18 17:25:44 -0500

    modular

--- Views/PopUpView.swift
+++ Views/PopUpView.swift
@@ -34,95 +34,115 @@ struct PopUpView: View {
         case .empty:
             EmptyView()
         case .themeMakerGuide:
-            VStack(spacing: 30) {
-                Text("Exploring the Theme Maker")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .padding(.horizontal, 150)
-                    .padding(.top, 44)
-                    .multilineTextAlignment(.center)
-                    .themedForeground(using: theme, in: colorScheme)
-
-                ScrollView {
-                    LazyVStack(alignment: .leading, spacing: 36) {
-                        HStack(spacing: 16) {
-                            Image(systemName: "pencil.and.outline")
-                                .font(.title)
-                                .foregroundStyle(themeColor(from: theme, for: .primary, in: colorScheme, level: 1))
-                                .frame(width: 36, height: 36)
-                            VStack(alignment: .leading) {
-                                Text("Saving and Applying Themes")
-                                    .fontWeight(.semibold)
-                                    .font(.subheadline)
-                                    .foregroundStyle(themeColor(from: theme, for: .primary, in: colorScheme, level: 0))
-                                Text("Save your custom theme with ease and reuse it anytime with just a tap.")
-                                    .font(.subheadline)
-                                    .opacity(0.7)
-                                    .foregroundStyle(themeColor(from: theme, for: .primary, in: colorScheme, level: 0))
-                            }
-                        }
-
-                        HStack(spacing: 16) {
-                            Image(systemName: "paintpalette.fill")
-                                .font(.title)
-                                .foregroundStyle(themeColor(from: theme, for: .primary, in: colorScheme, level: 1))
-                                .frame(width: 36, height: 36)
-                            VStack(alignment: .leading) {
-                                Text("Editing Accent Colors")
-                                    .fontWeight(.semibold)
-                                    .font(.subheadline)
-                                    .foregroundStyle(themeColor(from: theme, for: .primary, in: colorScheme, level: 0))
-                                Text("Edit three accent colors with ease. WorkSpace adapts brightness and saturation to ensure your colors always look great!")
-                                    .font(.subheadline)
-                                    .opacity(0.7)
-                                    .foregroundStyle(themeColor(from: theme, for: .primary, in: colorScheme, level: 0))
-                            }
-                        }
-
-                        HStack(spacing: 16) {
-                            Image(systemName: "line.3.crossed.swirl.circle.fill")
-                                .font(.title)
-                                .foregroundStyle(themeColor(from: theme, for: .primary, in: colorScheme, level: 1))
-                                .frame(width: 36, height: 36)
-                            VStack(alignment: .leading) {
-                                Text("Playing with Gradient")
-                                    .fontWeight(.semibold)
-                                    .font(.subheadline)
-                                    .foregroundStyle(themeColor(from: theme, for: .primary, in: colorScheme, level: 0))
-                                Text("Create stunning backgrounds with linear, radial, conic, or mesh gradients. WorkSpace’s playground simplifies the process, and its smart color algorithm guarantees gorgeous results.")
-                                    .font(.subheadline)
-                                    .opacity(0.7)
-                                    .foregroundStyle(themeColor(from: theme, for: .primary, in: colorScheme, level: 0))
-                            }
-                        }
+            PopUpContent(
+                title: "Exploring the Theme Maker",
+                items: [
+                    PopUpItem(
+                        icon: "pencil.and.outline",
+                        title: "Saving and Applying Themes",
+                        description: "Save your custom theme with ease and reuse it anytime with just a tap."
+                    ),
+                    PopUpItem(
+                        icon: "paintpalette.fill",
+                        title: "Editing Accent Colors",
+                        description: "Edit three accent colors with ease. WorkSpace adapts brightness and saturation to ensure your colors always look great!"
+                    ),
+                    PopUpItem(
+                        icon: "line.3.crossed.swirl.circle.fill",
+                        title: "Playing with Gradient",
+                        description: "Create stunning backgrounds with linear, radial, conic, or mesh gradients. WorkSpace’s playground simplifies the process, and its smart color algorithm guarantees gorgeous results."
+                    )
+                ],
+                buttonText: "Got it!",
+                buttonAction: {
+                    withAnimation(.spring()) {
+                        popUp.type = .empty
                     }
-                    .padding(.horizontal, 140)
                 }
-
-                Spacer()
-
-                Button(action: { withAnimation(.spring()) {
-                    popUp.type = .empty
-                }}) {
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 16)
-                            .fill(themeColor(from: theme, in: colorScheme, level: 1))
-                            .frame(height: 50)
-
-                        Text("Got it!")
-                            .fontWeight(.semibold)
-                            .themedForeground(using: theme, in: colorScheme, level: 5)
-                    }
-                    .shadow(color: theme.primary.toShadow(), radius: 12, y: 8)
-                }
-                .padding(.horizontal, 132)
-                .padding(.bottom, 56)
-            }
-            .frame(maxWidth: 618, maxHeight: 800)
-            .background(LinearGradient(colors: [themeColor(from: theme, for: .primary, in: colorScheme, level: 4), themeColor(from: theme, for: .primary, in: colorScheme, level: 5)], startPoint: .top, endPoint: .bottom))
-            .cornerRadius(16)
-            .shadow(color: theme.primary.toShadow(), radius: 16, y: 8)
-            .padding(48)
+            )
         }
     }
+}
+
+struct PopUpContent: View {
+    let title: String
+    let items: [PopUpItem]
+    let buttonText: String
+    let buttonAction: () -> Void
+
+    @Environment(\.theme) private var theme
+    @Environment(\.colorScheme) private var colorScheme
+
+    var body: some View {
+        VStack(spacing: 30) {
+            Text(title)
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding(.horizontal, 150)
+                .padding(.top, 44)
+                .multilineTextAlignment(.center)
+                .themedForeground(using: theme, in: colorScheme)
+
+            ScrollView {
+                LazyVStack(alignment: .leading, spacing: 36) {
+                    ForEach(items, id: \.id) { item in
+                        HStack(spacing: 16) {
+                            Image(systemName: item.icon)
+                                .font(.title)
+                                .foregroundStyle(themeColor(from: theme, for: .primary, in: colorScheme, level: 1))
+                                .frame(width: 36, height: 36)
+                            VStack(alignment: .leading) {
+                                Text(item.title)
+                                    .fontWeight(.semibold)
+                                    .font(.subheadline)
+                                    .foregroundStyle(themeColor(from: theme, for: .primary, in: colorScheme, level: 0))
+                                Text(item.description)
+                                    .font(.subheadline)
+                                    .opacity(0.7)
+                                    .foregroundStyle(themeColor(from: theme, for: .primary, in: colorScheme, level: 0))
+                            }
+                        }
+                    }
+                }
+                .padding(.horizontal, 140)
+            }
+
+            Spacer()
+
+            Button(action: buttonAction) {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 16)
+                        .fill(themeColor(from: theme, in: colorScheme, level: 1))
+                        .frame(height: 50)
+
+                    Text(buttonText)
+                        .fontWeight(.semibold)
+                        .themedForeground(using: theme, in: colorScheme, level: 5)
+                }
+                .shadow(color: theme.primary.toShadow(), radius: 12, y: 8)
+            }
+            .padding(.horizontal, 132)
+            .padding(.bottom, 56)
+        }
+        .frame(maxWidth: 618, maxHeight: 800)
+        .background(
+            LinearGradient(
+                colors: [
+                    themeColor(from: theme, for: .primary, in: colorScheme, level: 4),
+                    themeColor(from: theme, for: .primary, in: colorScheme, level: 5)
+                ],
+                startPoint: .top, endPoint: .bottom
+            )
+        )
+        .cornerRadius(16)
+        .shadow(color: theme.primary.toShadow(), radius: 16, y: 8)
+        .padding(48)
+    }
+}
+
+struct PopUpItem: Identifiable {
+    let id = UUID()
+    let icon: String
+    let title: String
+    let description: String
 }
