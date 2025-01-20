dce02d34789282bf46aafb2d6649da2a07d6025c
zimoluo <zimo_luo@outlook.com>
2025-01-19 21:49:06 -0500

    my thoughts on the data structure of our beloved windows

--- Models/Window.swift
+++ Models/Window.swift
@@ -4,28 +4,29 @@ import SwiftUI
 struct Window {
     var id: UUID
 
-    var x: CGFloat
-    var y: CGFloat
-    var width: CGFloat
-    var height: CGFloat
+    var state: WindowState
     var data: WindowData
 
     init(
-        x: CGFloat = 0,
-        y: CGFloat = 0,
-        width: CGFloat = 100,
-        height: CGFloat = 100,
-        data: WindowData = .init()
+        state: WindowState = .init(),
+        data: WindowData = .init(),
+        order: Int = 0
     ) {
         self.id = UUID()
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
+        self.state = state
         self.data = data
     }
 }
 
+// WindowState stores the dimension and position of the window. Due to its changeable nature, it's managed by windows themselves, but it's also stored in model context in order to persist. Windows will choose specific occasions, like right after finishing dragging, to sync its version of WindowState with the model context. This means individual windows only need its own copy of state and its UUID which is used to link itself to the model context.
+struct WindowState {
+    var x: CGFloat = 0
+    var y: CGFloat = 0
+    var width: CGFloat = 100
+    var height: CGFloat = 100
+}
+
+// WindowData is directly tied to the model context, meaning that individual windows won't keep their own version of the window data, and every change happens directly at the app data level. It's still mutable, but much less needed compared to WindowState, so the performance impact is minimal.
 struct WindowData {
     var type: WindowType
     var saveData: [String: String]
@@ -34,6 +35,7 @@ struct WindowData {
     var maxHeight: CGFloat
     var maxWidth: CGFloat
     var layer: Int
+    var order: Int
 
     init(
         type: WindowType = .blank,
@@ -42,7 +44,8 @@ struct WindowData {
         minWidth: CGFloat = 0,
         maxHeight: CGFloat = .infinity,
         maxWidth: CGFloat = .infinity,
-        layer: Int = 0
+        layer: Int = 0,
+        order: Int = 0
     ) {
         self.type = type
         self.saveData = saveData
@@ -51,10 +54,36 @@ struct WindowData {
         self.maxHeight = maxHeight
         self.maxWidth = maxWidth
         self.layer = layer
+        self.order = order
     }
 }
 
 enum WindowType {
     case blank
     case notes
+
+    func presetData() -> WindowData {
+        switch self {
+        case .blank:
+            return WindowData(
+                type: .blank,
+                saveData: [:],
+                minHeight: 0,
+                minWidth: 0,
+                maxHeight: .infinity,
+                maxWidth: .infinity,
+                layer: 0
+            )
+        case .notes:
+            return WindowData(
+                type: .notes,
+                saveData: [:],
+                minHeight: 200,
+                minWidth: 300,
+                maxHeight: 300,
+                maxWidth: 500,
+                layer: 1
+            )
+        }
+    }
 }
