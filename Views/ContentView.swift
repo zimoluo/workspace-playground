d167e8af034a5a51100b83105c807ecd460b1ae7
zimoluo <zimo_luo@outlook.com>
2025-01-26 17:44:24 -0500

    tertiary color

--- Views/ContentView.swift
+++ Views/ContentView.swift
@@ -239,7 +239,7 @@ struct ContentView: View {
                 Image(systemName: "sidebar.leading")
                     .font(.system(size: 24))
                     .padding(16)
-                    .foregroundColor(columnVisibility == .detailOnly ? themeColor(from: theme, for: .secondary, in: colorScheme, level: 0) : themeColor(from: theme, for: .primary, in: colorScheme, level: 0))
+                    .foregroundColor(columnVisibility == .detailOnly ? (selectedScreen.type == .space ? themeColor(from: theme, for: .tertiary, in: colorScheme, level: 0) : themeColor(from: theme, for: .secondary, in: colorScheme, level: 0)) : themeColor(from: theme, for: .primary, in: colorScheme, level: 0))
             }
             .position(x: columnVisibility == .detailOnly ? 36 : 32, y: 24)
 
