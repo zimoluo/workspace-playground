114fe866c5ed43b3abb62cf60a52eb63490ffdf4
zimoluo <zimo_luo@outlook.com>
2025-02-09 15:57:04 -0500

    adding in all the customization

--- Utilities/SettingsProvider.swift
+++ Utilities/SettingsProvider.swift
@@ -47,15 +47,25 @@ struct SettingsProvider<Content: View>: View {
             modelContext.insert(theme)
         }
 
-        var firstSpace = BuiltinSpaces.productivity
-        var personalSpace = BuiltinSpaces.personal
-        var writingSpace = BuiltinSpaces.writing
-        var doodleSpace = BuiltinSpaces.doodle
+        let firstSpace = BuiltinSpaces.productivity
+        let personalSpace = BuiltinSpaces.personal
+        let writingSpace = BuiltinSpaces.writing
+        let doodleSpace = BuiltinSpaces.doodle
+
+        firstSpace.dateCreated = Calendar.current.date(byAdding: .minute, value: -90, to: Date()) ?? Date()
+        writingSpace.dateCreated = Calendar.current.date(byAdding: .minute, value: -120, to: Date()) ?? Date()
+        personalSpace.dateCreated = Calendar.current.date(byAdding: .minute, value: -150, to: Date()) ?? Date()
+        doodleSpace.dateCreated = Calendar.current.date(byAdding: .minute, value: -180, to: Date()) ?? Date()
+
+        firstSpace.dateModified = Calendar.current.date(byAdding: .minute, value: -90, to: Date()) ?? Date()
+        writingSpace.dateModified = Calendar.current.date(byAdding: .minute, value: -120, to: Date()) ?? Date()
+        personalSpace.dateModified = Calendar.current.date(byAdding: .minute, value: -150, to: Date()) ?? Date()
+        doodleSpace.dateModified = Calendar.current.date(byAdding: .minute, value: -180, to: Date()) ?? Date()
 
         let defaultSpaces: [Space] = [
-            BuiltinSpaces.doodle,
-            BuiltinSpaces.personal,
-            BuiltinSpaces.writing,
+            doodleSpace,
+            personalSpace,
+            writingSpace,
             firstSpace,
         ]
 
@@ -67,6 +77,16 @@ struct SettingsProvider<Content: View>: View {
             try modelContext.save()
             UserDefaults.standard.set(true, forKey: "HasInitializedDefaults")
             currentSettings.selectedSpaceId = firstSpace.id
+            currentSettings.habits = [
+                Habit(id: UUID(), name: "Writing ✏️", dateCreated: Calendar.current.date(byAdding: .day, value: -60, to: Date()) ?? Date(), dateModified: Calendar.current.date(byAdding: .day, value: -60, to: Date()) ?? Date(), timesKept: [Date(), Date(), Date(), Date()], timesDisliked: []),
+                Habit(id: UUID(), name: "Coding 🧑‍💻", dateCreated: Calendar.current.date(byAdding: .day, value: -75, to: Date()) ?? Date(), dateModified: Calendar.current.date(byAdding: .day, value: -75, to: Date()) ?? Date(), timesKept: [Date(), Date(), Date(), Date(), Date(), Date()], timesDisliked: []),
+                Habit(id: UUID(), name: "Hiking 🥾", dateCreated: Calendar.current.date(byAdding: .day, value: -111, to: Date()) ?? Date(), dateModified: Calendar.current.date(byAdding: .day, value: -111, to: Date()) ?? Date(), timesKept: [Date(), Date(), Date(), Date(), Date()], timesDisliked: []),
+            ]
+            currentSettings.importantDays = [
+                ImportantDay(id: UUID(), name: "WWDC 2024", date: Calendar.current.date(from: DateComponents(timeZone: TimeZone(abbreviation: "PDT"), year: 2024, month: 6, day: 10, hour: 10, minute: 0)) ?? Date()),
+                ImportantDay(id: UUID(), name: "WWDC 2023", date: Calendar.current.date(from: DateComponents(timeZone: TimeZone(abbreviation: "PDT"), year: 2023, month: 6, day: 5, hour: 10, minute: 0)) ?? Date()),
+                ImportantDay(id: UUID(), name: "WWDC 2022", date: Calendar.current.date(from: DateComponents(timeZone: TimeZone(abbreviation: "PDT"), year: 2022, month: 6, day: 6, hour: 10, minute: 0)) ?? Date()),
+            ]
         } catch {
             print("Error initializing default data: \(error)")
         }
