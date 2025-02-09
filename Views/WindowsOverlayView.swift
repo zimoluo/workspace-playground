9910ee0df091acf9a37a117a3ec13dea8d8e97a7
zimoluo <zimo_luo@outlook.com>
2025-02-09 14:01:50 -0500

    further reduce it

--- Views/WindowsOverlayView.swift
+++ Views/WindowsOverlayView.swift
@@ -3,7 +3,7 @@ import SwiftUI
 struct WindowsOverlayView: View {
     @ObservedObject var space: Space
     let parentSize: CGSize
-    let cullingBuffer: CGFloat = 30
+    let cullingBuffer: CGFloat = 16
 
     var body: some View {
         let totalOffset = CGPoint(x: -space.cameraCenterX, y: -space.cameraCenterY)
